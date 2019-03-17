7cc7ebf906ac15c51fa4b0aa67dc42cccbe925bc
Chris <cl2871@nyu.edu>
2019-03-17 14:18:00 -0400

    Initial Commit

---
+++ src/test/java/com/pillowfruit/eloservice/EloServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pillowfruit.eloservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EloServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
